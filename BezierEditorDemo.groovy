4f76741ab2e6712fc6666623134499a0927e7ea3
Kevin Harrington <mad.hephaestus@gmail.com>
2023-01-16 18:01:22 -0500

    cleanup

--- BezierEditorDemo.groovy
+++ BezierEditorDemo.groovy
@@ -3,14 +3,10 @@ import com.neuronrobotics.bowlerkernel.Bezier3d.*;
 import eu.mihosoft.vrl.v3d.*;
 def URL="https://gist.github.com/4aeeaa49bd3a807eed8f8ff3dea84c48.git"
 def file="bez.json"
-//Temp file
-//BezierEditor editor = new BezierEditor(
-//		new File(ScriptingEngine.getAppData().getAbsolutePath()+"/bez2.json")
-//		,20)
 
 //Git stored file loaded but not saved
 BezierEditor editor = new BezierEditor(ScriptingEngine.fileFromGit(URL, file),20)
-//Git file loaded and saved
+//Git file loaded and saved. THis will do a git call on each event of change
 //BezierEditor editor = new BezierEditor(URL, file,10)
 
 ArrayList<Transform>  transforms = editor.transforms()
